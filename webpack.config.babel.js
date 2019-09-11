35b9ffebfa7813a10bfd9f336fa4676b3348d3a4
Christoph Stenglein <christoph-tobias.stenglein@r-software.at>
2019-09-11 14:37:22 +0200

    fix some linting issues and format code

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,143 +1,143 @@
-'use strict'
+'use strict';
 
-const webpack = require('webpack')
-const path = require('path')
-const buildPath = path.join(__dirname, './dist')
-const args = require('yargs').argv
+const webpack = require('webpack');
+const path = require('path');
+const buildPath = path.join(__dirname, './dist');
+const args = require('yargs').argv;
 
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const postcssPresetEnv = require('postcss-preset-env')
-const cssnano = require('cssnano')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const postcssPresetEnv = require('postcss-preset-env');
+const cssnano = require('cssnano');
 
-let isProd = args.prod
-let isDev = args.dev
-let env = args.envFile
+let isProd = args.prod;
+let isDev = args.dev;
+let env = args.envFile;
 if (env) {
-  // Load env file
-  require('dotenv').config({ path: env })
+    // Load env file
+    require('dotenv').config({path: env})
 }
 
-let main = ['./src/site.js']
-let common = ['./src/common.js']
-let devtool
+let main = ['./src/site.js'];
+let common = ['./src/common.js'];
+let devtool;
 
 if (isDev) {
-  main.push('webpack-dev-server/client?http://0.0.0.0:8080')
-  devtool = 'source-map'
+    main.push('webpack-dev-server/client?http://0.0.0.0:8080');
+    devtool = 'source-map'
 }
 
 let plugins = [
-  new MiniCssExtractPlugin({ filename: '[name].[hash].css' }),
-  new HtmlWebpackPlugin({
-    template: './src/index.html',
-    chunks: ['main'],
-    inject: 'body'
-  }),
-  new HtmlWebpackPlugin({
-    template: './src/error.html',
-    chunks: ['common'],
-    inject: 'body',
-    filename: 'error.html'
-  }),
-  new webpack.DefinePlugin({
-    'process.env.CLIENT_ID': JSON.stringify(process.env.CLIENT_ID),
-    'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
-  })
-]
+    new MiniCssExtractPlugin({filename: '[name].[hash].css'}),
+    new HtmlWebpackPlugin({
+        template: './src/index.html',
+        chunks: ['main'],
+        inject: 'body'
+    }),
+    new HtmlWebpackPlugin({
+        template: './src/error.html',
+        chunks: ['common'],
+        inject: 'body',
+        filename: 'error.html'
+    }),
+    new webpack.DefinePlugin({
+        'process.env.CLIENT_ID': JSON.stringify(process.env.CLIENT_ID),
+        'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
+    })
+];
 
 if (isProd) {
-  plugins.push(
-    new webpack.NoEmitOnErrorsPlugin()
-  )
+    plugins.push(
+        new webpack.NoEmitOnErrorsPlugin()
+    )
 }
 
 module.exports = {
-  entry: {
-    'main': main,
-    'common': common
-  },
-  node: {
-    fs: 'empty',
-    net: 'empty',
-    tls: 'empty',
-    child_process: 'empty'
-  },
+    entry: {
+        'main': main,
+        'common': common
+    },
+    node: {
+        fs: 'empty',
+        net: 'empty',
+        tls: 'empty',
+        child_process: 'empty'
+    },
 
-  output: {
-    path: buildPath,
-    publicPath: '/',
-    filename: '[name].[hash].js'
-  },
+    output: {
+        path: buildPath,
+        publicPath: '/',
+        filename: '[name].[hash].js'
+    },
 
-  module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: [
-        {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              [
-                '@babel/preset-env',
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: [
                 {
-                  targets: {
-                    firefox: 24,
-                    ie: 11
-                  },
-                  useBuiltIns: 'usage',
-                  corejs: 3,
-                  debug: true
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            [
+                                '@babel/preset-env',
+                                {
+                                    targets: {
+                                        firefox: 24,
+                                        ie: 11
+                                    },
+                                    useBuiltIns: 'usage',
+                                    corejs: 3,
+                                    debug: true
+                                }
+                            ]
+                        ],
+                        plugins: ['@babel/plugin-transform-modules-commonjs']
+                    }
                 }
-              ]
-            ],
-            plugins: ['@babel/plugin-transform-modules-commonjs']
-          }
-        }
-      ]
+            ]
+        },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: ['style-loader', MiniCssExtractPlugin.loader, {
+                    loader: 'css-loader',
+                    options: {importLoaders: 1}
+                },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            ident: 'postcss',
+                            plugins: () => [
+                                postcssPresetEnv({browsers: 'last 2 versions'}),
+                                cssnano({preset: ['default', {discardComments: {removeAll: true}}]})
+                            ]
+                        }
+                    }, 'sass-loader']
+            },
+            {
+                test: /\.(eot|svg|ttf|woff|woff2)$/,
+                loader: 'file-loader?name=images/[name].[ext]'
+            },
+            {
+                test: /\.(png|jpg|ico)$/,
+                exclude: /node_modules/,
+                use: [{loader: 'file-loader?name=images/[name].[ext]&context=./src/images'}]
+            },
+            {
+                test: require.resolve('jquery'),
+                use: [{loader: 'expose-loader', options: 'jQuery'}, {loader: 'expose-loader', options: '$'}]
+            }
+        ]
     },
-    {
-      test: /\.scss$/,
-      exclude: /node_modules/,
-      use: ['style-loader', MiniCssExtractPlugin.loader, {
-        loader: 'css-loader',
-        options: { importLoaders: 1 }
-      },
-      {
-        loader: 'postcss-loader',
-        options: {
-          ident: 'postcss',
-          plugins: () => [
-            postcssPresetEnv({ browsers: 'last 2 versions' }),
-            cssnano({ preset: ['default', { discardComments: { removeAll: true } }] })
-          ]
-        }
-      }, 'sass-loader']
-    },
-    {
-      test: /\.(eot|svg|ttf|woff|woff2)$/,
-      loader: 'file-loader?name=images/[name].[ext]'
-    },
-    {
-      test: /\.(png|jpg|ico)$/,
-      exclude: /node_modules/,
-      use: [{ loader: 'file-loader?name=images/[name].[ext]&context=./src/images' }]
-    },
-    {
-      test: require.resolve('jquery'),
-      use: [{ loader: 'expose-loader', options: 'jQuery' }, { loader: 'expose-loader', options: '$' }]
+
+    plugins: plugins,
+
+    devtool: devtool,
+
+    devServer: {
+        contentBase: buildPath,
+        host: '0.0.0.0',
+        port: 8080
     }
-    ]
-  },
-
-  plugins: plugins,
-
-  devtool: devtool,
-
-  devServer: {
-    contentBase: buildPath,
-    host: '0.0.0.0',
-    port: 8080
-  }
-}
+};
