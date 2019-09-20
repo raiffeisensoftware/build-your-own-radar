a66bfe062a9309ae5b356f91996548a474382689
Christoph Stenglein <christoph-tobias.stenglein@r-software.at>
2019-09-20 15:24:19 +0200

    removed radar_legend.png

--- src/common.js
+++ src/common.js
@@ -3,4 +3,3 @@ require('./images/tech-radar-landing-page-wide.png');
 require('./images/tw-logo.png');
 require('./images/favicon.ico');
 require('./images/search-logo-2x.svg');
-require('./images/radar_legend.png');
