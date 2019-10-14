c34bd49be74151ffadefe6f6d60d023fab6bf51b
Christoph Stenglein <christoph-tobias.stenglein@r-software.at>
2019-10-14 15:49:06 +0200

    remove tw-logo

--- src/common.js
+++ src/common.js
@@ -1,6 +1,5 @@
 import './stylesheets/base.scss';
 import './images/tech-radar-landing-page-wide.png';
-import './images/tw-logo.png';
 import './images/favicon.ico';
 import './images/search-logo-2x.svg';
 import './images/headercomp.png';
